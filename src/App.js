d5742cb2f71160885b96cb2a31fd7c5f0e733f28
Ankit9835 <ankydchrismatic@gmail.com>
2025-02-09 17:22:16 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
+
 function App() {
+  cont [cartShown,setCartIsShown] = useState(true)
   return (
     <CartProvider>
+      {cartShown && <div>cart...</div>}
       <Header />
       <main>
         <Meals />
